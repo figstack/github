e506e60baa35293e9fa87cbf4b21e56e6b06c287
Hahnbee Lee <hl985@cornell.edu>
2021-09-17 19:13:16 -0400

    just for reference

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
+  // app.on("issue_comment.created", async (context) => {
+  //   const issueComment = context.issue({
+  //     body: "issue comment!",
+  //   });
+  //   console.log(context);
+  //   await context.octokit.issues.createComment(issueComment);
+  // });
+
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
+
+  app.on('pull_request.opened', async context =>{
+    const pr =context.payload.pull_request
+    const user = pr.user.login //Collecting Details of the person who created the PR
+    const msg=context.issue({body:'Hey ' + user + ' 👋, Thanks for the PR !!! You are Awesome.'})
+    return context.octokit.issues.createComment(msg)
+  })
+
+  app.on("pull_request_review_comment.created", async (context) => {
+    const issueComment = context.issue({
+      body: "pr commentt",
+    });
+    await context.octokit.issues.createComment(issueComment);
+    console.log(context);
+    console.log(context);
+  })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
