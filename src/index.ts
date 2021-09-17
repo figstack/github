87640e3a3ef81865ed291b2e07513f2bf4dafdbc
Hahnbee Lee <hl985@cornell.edu>
2021-09-17 19:45:35 -0400

    Only pull request comment

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,6 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  // app.on("issue_comment.created", async (context) => {
-  //   const issueComment = context.issue({
-  //     body: "issue comment!",
-  //   });
-  //   console.log(context);
-  //   await context.octokit.issues.createComment(issueComment);
-  // });
-
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-
-  app.on('pull_request.opened', async context =>{
-    const pr =context.payload.pull_request
-    const user = pr.user.login //Collecting Details of the person who created the PR
-    const msg=context.issue({body:'Hey ' + user + ' 👋, Thanks for the PR !!! You are Awesome.'})
-    return context.octokit.issues.createComment(msg)
-  })
-
   app.on("pull_request_review_comment.created", async (context) => {
     const issueComment = context.issue({
       body: "pr commentt",
