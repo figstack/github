53c8adc33bffcd416989bd65f8331371b8905f35
Hahnbee Lee <hl985@cornell.edu>
2021-09-19 00:02:54 -0400

    rip threading

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const IS_DEV = true;
+export const FRONTEND_ENDPOINT = IS_DEV ? 'http://localhost:3000' : 'https://figstack.com';
+export const BACKEND_ENDPOINT = IS_DEV ? 'http://localhost:5000' : 'https://api.figstack.com';
+
+// URL config for authentication
+const auth0Domain = IS_DEV ? 'https://dev-uxa1yxhj.us.auth0.com' : 'https://figstack.us.auth0.com';
+const responseType = 'github';
+const clientId = IS_DEV ? 'nv8BC1pmSBIw2HMNRqsd8Bkl5xwc1ipN' : 'zyVI6tCd7UQ44NCkqlx3TsulhrLtMYzm'
+const scope = 'openid profile email offline_access';
+// Scheme is used to distinguish between VS Code and VS Code Internals
+export const getLoginURL = (uriScheme: string) => {
+  const redirectUri = `${FRONTEND_ENDPOINT}/api/auth/${uriScheme}/login`;
+  return `${auth0Domain}/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+};
+
+export const getLogoutURL = (uriScheme: string) => {
+  const returnToUri = `${FRONTEND_ENDPOINT}/api/auth/${uriScheme}/logout`;
+  return `${auth0Domain}/v2/logout?client_id=${clientId}&returnTo=${returnToUri}`;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
 import { Probot } from "probot";
+// import axios from 'axios';
+// import { BACKEND_ENDPOINT } from './constants';
 
 export = (app: Probot) => {
   app.on("pull_request_review_comment.created", async (context) => {
-    const issueComment = context.issue({
-      body: "pr commentt",
-    });
-    await context.octokit.issues.createComment(issueComment);
-    console.log(context);
-    console.log(context);
+    // const originalComment = context.payload.comment.body;
+    // const { owner } = context.payload.repository
+    const newComment = {
+      pull_number: context.payload.pull_request.id,
+      repo: context.payload.repository.name,
+      comment_id: context.payload.comment.id,
+      owner: context.payload.repository.owner.login,
+      body: 'THREADDD'
+    }
+    
+    // try {
+    //   const { refreshToken, accessToken } = getTokens();
+    //   switch(originalComment) {
+    //     case 'figstack explain':
+    //       break;
+    //     case 'figstack time complexity':
+    //       break;
+    //     default:
+    //       // parse if question is asked
+    //       const index = originalComment.indexOf("figstack ask ")
+    //       if (index != -1) {
+    //         const question = originalComment.substring(index+13)
+    //         const askResponse = await axios.post(`${BACKEND_ENDPOINT}/function/v1/ask`, {
+    //           code: context.payload.comment.diff_hunk,
+    //           question,
+    //           accessToken,
+    //           refreshToken,
+    //           source: 'github'
+    //         });
+    //         const { output } = askResponse.data;
+    //         newComment.body = output
+    //       }
+    //       break;
+    //   }
+    // } catch (err: any) {
+    //   //
+    // }
+    try {
+      await context.octokit.pulls.createReplyForReviewComment(newComment);
+    } catch (err: any) {
+      console.log(err)
+    }
+
   })
 
   // For more information on building apps:
