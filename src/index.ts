be3de456cc13a7eef2d4d737a5f1acedd435b209
Han Wang <hyw2@cornell.edu>
2021-09-27 02:12:28 -0400

    Add support for username github auth

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { Probot, Context } from "probot";
 import axios from 'axios';
 import { BACKEND_ENDPOINT } from './constants';
-const ACCESS_TOKEN = "PxyqEHcpLogOyEIUaByzdy6vyISIzhdf";
-const SOURCE_GITHUB = 'github'
+const SOURCE_GITHUB = 'github';
 
-const format = (ogComment: String, header: String, edit: String) => {
+const parseCode = (ogCode: string) => {
+  const trimmedCode = ogCode.trim();
+  const lines = trimmedCode.split('\n');
+  lines.splice(0,1);
+
+  const parsedLines = lines.map((line) => {
+    const firstCharacter = line.charAt(0);
+    if (firstCharacter === '-' || firstCharacter === '+') {
+      return line.substring(1);
+    }
+
+    return line;
+  })
+
+  return parsedLines.join('\n');
+}
+
+const format = (ogComment: string, header: string, edit: string): string => {
   const formatHeader = "#### " + header + "\n"
   return (
     ogComment + "\n\n" + formatHeader + edit
@@ -13,16 +29,58 @@ const format = (ogComment: String, header: String, edit: String) => {
 
 export = (app: Probot) => {
   app.on("pull_request_review_comment.created", async (context: Context) => {
-    const comment = context.payload.comment.body;
-    const code = context.payload.comment.diff_hunk;
+    const comment = context.payload.comment.body.trim();
+    const code = parseCode(context.payload.comment.diff_hunk);
     let issueComment = {
       body: format(comment, "Explanation", "EDIT"),
       comment_id: context.payload.comment.id,
       owner: context.payload.repository.owner.login,
       repo: context.payload.repository.name,
     };
+
+    const language = context.payload.repository.language;
+    const githubUsername = context.payload.comment.user.login;
     
     try {
+      switch(comment) {
+        case '/explain':
+          const explainResponse = await axios.post(`${BACKEND_ENDPOINT}/function/v1/explain`, {
+            code,
+            inputLanguage: language,
+            outputLanguage: 'English',
+            githubUsername,
+            source: SOURCE_GITHUB
+          });
+          let explainHeader = "Explanation"
+          issueComment.body = format(comment, explainHeader, explainResponse.data.output)
+          break;
+        case '/complexity':
+          const complexityResponse = await axios.post(`${BACKEND_ENDPOINT}/function/v1/complexity`, {
+            code,
+            inputLanguage: language,
+            githubUsername,
+            source: SOURCE_GITHUB
+          });
+          let timeComplexityHeader = "Time Complexity"
+          issueComment.body = format(comment, timeComplexityHeader, complexityResponse.data.output)
+          break;
+        default:
+          // parse if question is asked
+          const index = comment.indexOf("/ask ")
+          if (index != -1) {
+            const question = comment.substring(index+13)
+            const askResponse = await axios.post(`${BACKEND_ENDPOINT}/function/v1/ask`, {
+              code,
+              question,
+              inputLanguage: language,
+              githubUsername,
+              source: SOURCE_GITHUB
+            });
+            let askHeader = "Answer"
+            issueComment.body = format(comment, askHeader, askResponse.data.output)
+          }
+          break;
+      }
     } catch (err: any) {
       console.log(err)
     }
